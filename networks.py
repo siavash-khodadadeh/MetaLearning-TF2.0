69f8f9fa0d10606a12769a4be4587ec905fb0bdd
Sharare <sharare.zethabian@gmail.com>
2020-01-09 12:48:29 -0500

    Prototypical networks added to the framework.

--- networks.py
+++ networks.py
@@ -4,6 +4,38 @@ import time
 from tensorflow.keras.layers import Conv2D, Flatten, Dense, Input, BatchNormalization
 
 
+class SimpleModelProto(tf.keras.Model):
+    name = 'SimpleModelProto'
+
+    def __init__(self):
+        super(SimpleModelProto, self).__init__(name='simple_model')
+
+        self.conv1 = tf.keras.layers.Conv2D(64, 3, name='conv1', strides=(2, 2), padding='same')
+        self.bn1 = tf.keras.layers.BatchNormalization(momentum=0.0, center=True, scale=False, name='bn1')
+        self.conv2 = tf.keras.layers.Conv2D(64, 3, name='conv2', strides=(2, 2), padding='same')
+        self.bn2 = tf.keras.layers.BatchNormalization(momentum=0.0, center=True, scale=False, name='bn2')
+        self.conv3 = tf.keras.layers.Conv2D(64, 3, name='conv3', strides=(2, 2), padding='same')
+        self.bn3 = tf.keras.layers.BatchNormalization(momentum=0.0, center=True, scale=False, name='bn3')
+        self.conv4 = tf.keras.layers.Conv2D(64, 3, name='conv4',  strides=(2, 2), padding='same')
+        self.bn4 = tf.keras.layers.BatchNormalization(momentum=0.0, center=True, scale=False, name='bn4')
+        self.flatten = Flatten(name='flatten')
+
+    def conv_block(self, features, conv, bn=None, training=False):
+        conv_out = conv(features)
+        batch_normalized_out = bn(conv_out, training=training)
+        return tf.keras.activations.relu(batch_normalized_out)
+
+    def call(self, inputs, training=False):
+        image = inputs
+        c1 = self.conv_block(image, self.conv1, self.bn1, training=training)
+        c2 = self.conv_block(c1, self.conv2, self.bn2, training=training)
+        c3 = self.conv_block(c2, self.conv3, self.bn3, training=training)
+        c4 = self.conv_block(c3, self.conv4, self.bn4, training=training)
+        f = self.flatten(c4)
+
+        return f
+
+
 class SimpleModel(tf.keras.Model):
     name = 'SimpleModel'
 
