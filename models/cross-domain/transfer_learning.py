1e8bd8a27d551441740558923e6d6f6752a50b42
siavash <siavash.khodadadeh@gmail.com>
2020-04-03 16:51:19 -0400

    Use input shape 224x224 for transfer-learning.

--- models/cross-domain/transfer_learning.py
+++ models/cross-domain/transfer_learning.py
@@ -16,6 +16,7 @@ class MiniImagenetDatabasePreProcess(MiniImagenetDatabase):
         def parse_function(example_address):
             image = tf.image.decode_jpeg(tf.io.read_file(example_address))
             image = tf.cast(image, tf.float32)
+            image = tf.image.resize(image, self.input_shape[:2])
             image = tf.keras.applications.vgg16.preprocess_input(image)
             return image
 
@@ -23,7 +24,7 @@ class MiniImagenetDatabasePreProcess(MiniImagenetDatabase):
 
 
 def run_mini_imagenet():
-    mini_imagenet_database = MiniImagenetDatabasePreProcess()
+    mini_imagenet_database = MiniImagenetDatabasePreProcess(input_shape=(224, 224, 3))
     transfer_learning = TransferLearning(
         database=mini_imagenet_database,
         network_cls=get_transfer_net,
@@ -35,19 +36,19 @@ def run_mini_imagenet():
         k_test=5,
         meta_batch_size=4,
         num_steps_ml=5,
-        lr_inner_ml=0.05,
+        lr_inner_ml=0.005,
         num_steps_validation=5,
         save_after_iterations=15000,
         meta_learning_rate=0.001,
         report_validation_frequency=250,
         log_train_images_after_iteration=1000,
         number_of_tasks_val=100,
-        number_of_tasks_test=1000,
+        number_of_tasks_test=100,
         clip_gradients=True,
         experiment_name='fixed_vgg_16_with_three_layers'
     )
 
-    transfer_learning.train(iterations=60000)
+    # transfer_learning.train(iterations=60000)
     transfer_learning.evaluate(50, seed=14)
 
 
