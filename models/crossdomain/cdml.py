7b55220046685670f449d8726f8ce8dfef56ca7c
siavash <siavash.khodadadeh@gmail.com>
2020-04-28 19:56:32 -0400

    CDML baseline added. CUB dataset 2d samples are now automatically convert to 3d samples. acdml fixed to have meta batch size.

--- models/crossdomain/cdml.py
+++ models/crossdomain/cdml.py
@@ -8,11 +8,22 @@ from databases import MiniImagenetDatabase, PlantDiseaseDatabase, ISICDatabase, 
 from databases.data_bases import Database
 
 from typing import List
-from utils import keep_keys_with_greater_than_equal_k_items
 
 
-class AttentionCrossDomainMetaLearning(ModelAgnosticMetaLearningModel):
+class CombinedCrossDomainMetaLearning(ModelAgnosticMetaLearningModel):
     def get_train_dataset(self):
+        databases = [MiniImagenetDatabase(), AirplaneDatabase(), CUBDatabase()]
+
+        dataset = self.get_cross_domain_meta_learning_dataset(
+            databases=databases,
+            n=self.n,
+            k=self.k,
+            k_validation=self.k_val_ml,
+            meta_batch_size=self.meta_batch_size
+        )
+        return dataset
+
+    def get_val_dataset(self):
         databases = [MiniImagenetDatabase(), AirplaneDatabase(), CUBDatabase()]
 
         dataset = self.get_cross_domain_meta_learning_dataset(
@@ -52,7 +63,7 @@ class AttentionCrossDomainMetaLearning(ModelAgnosticMetaLearningModel):
                 n,
                 k,
                 k_validation,
-                meta_batch_size=2,
+                meta_batch_size=1,
                 one_hot_labels=one_hot_labels,
                 reshuffle_each_iteration=reshuffle_each_iteration,
                 seed=seed,
@@ -73,17 +84,14 @@ class AttentionCrossDomainMetaLearning(ModelAgnosticMetaLearningModel):
                 tensors.append(val_labels)
 
             def f(*args):
-                np.random.seed(42)
-                indices = np.random.choice(range(len(datasets)), size=2, replace=False)
-                tr_ds = args[indices[0] * 4][0, ...]
-                tr_labels = args[indices[0] * 4 + 2][0, ...]
-                tr_domain = args[indices[0] * 4][1, ...]
-                val_ds = args[indices[1] * 4 + 1][0, ...]
-                val_labels = args[indices[1] * 4 + 3][0, ...]
-                val_domain = args[indices[1] * 4][1, ...]
-                return tr_ds, tr_labels, tr_domain, val_ds, val_labels, val_domain
+                index = np.random.choice(range(len(datasets)), size=1, replace=False)[0]
+                tr_ds = args[index * 4]
+                val_ds = args[index * 4 + 1]
+                tr_labels = args[index * 4 + 2]
+                val_labels = args[index * 4 + 3]
+                return tr_ds, tr_labels, val_ds, val_labels
 
-            return tf.py_function(f, inp=tensors, Tout=[tf.string, tf.float32, tf.string] * 2)
+            return tf.py_function(f, inp=tensors, Tout=[tf.string, tf.float32] * 2)
 
         # TODO handle the seed
         parallel_iterations = None
@@ -91,10 +99,8 @@ class AttentionCrossDomainMetaLearning(ModelAgnosticMetaLearningModel):
         def parse_function(
             tr_task_imgs_addresses,
             tr_task_labels,
-            tr_domain_imgs_addresses,
             val_task_imgs_addresses,
             val_task_labels,
-            val_domain_imgs_addresses
         ):
             def parse_batch_imgs(img_addresses, shape):
                 img_addresses = tf.reshape(img_addresses, (-1,))
@@ -107,58 +113,46 @@ class AttentionCrossDomainMetaLearning(ModelAgnosticMetaLearningModel):
                 return tf.reshape(imgs, shape)
 
             tr_task_imgs = parse_batch_imgs(tr_task_imgs_addresses, (n, k, 84, 84, 3))
-            tr_dom_imgs = parse_batch_imgs(tr_domain_imgs_addresses, (n, k, 84, 84, 3))
             val_task_imgs = parse_batch_imgs(val_task_imgs_addresses, (n, k_validation, 84, 84, 3))
-            val_dom_imgs = parse_batch_imgs(val_domain_imgs_addresses, (n, k, 84, 84, 3))
+            tr_task_labels = tf.squeeze(tr_task_labels, axis=0)
+            val_task_labels = tf.squeeze(val_task_labels, axis=0)
 
-            return (tr_task_imgs, tr_dom_imgs, val_task_imgs, val_dom_imgs), (tr_task_labels, val_task_labels)
+            return (tr_task_imgs, val_task_imgs), (tr_task_labels, val_task_labels)
 
         dataset = tf.data.Dataset.zip(datasets)
         # TODO steps per epoch can be inferred from tf.data.experimental.cardinality(dataset)
         dataset = dataset.map(choose_two_domains)
         dataset = dataset.map(parse_function)
 
-        # TODO fix this for different meta batch sizes. Check to see if it works with removing the following line.
-        meta_batch_size = 1
         dataset = dataset.batch(batch_size=meta_batch_size, drop_remainder=False)
         steps_per_epoch = steps_per_epoch // meta_batch_size
 
         # import matplotlib.pyplot as plt
-        #
         # for item in dataset:
-        #     (tr_ds, tr_dom, val_ds, val_dom), (tr_labels, val_labels) = item
+        #
+        #     (tr_ds, val_ds), (tr_lbls, val_lbls) = item
         #     print(tr_ds.shape)
         #     print(val_ds.shape)
-        #     print(tr_dom.shape)
-        #     print(val_dom.shape)
-        #     print(tr_labels.shape)
-        #     print(val_labels.shape)
-        #
+        #     print(tr_lbls.shape)
+        #     print(val_lbls.shape)
         #     for i in range(n):
         #         for j in range(k):
         #             plt.imshow(tr_ds[i, j, ...])
         #             plt.show()
         #     for i in range(n):
-        #         for j in range(k):
-        #             plt.imshow(tr_dom[i, j, ...])
-        #             plt.show()
-        #     for i in range(n):
-        #         for j in range(k):
-        #             plt.imshow(val_dom[i, j, ...])
-        #             plt.show()
-        #     for i in range(n):
         #         for j in range(k_validation):
         #             plt.imshow(val_ds[i, j, ...])
         #             plt.show()
-        #     break
+        #     print(item)
+        #     exit()
 
         setattr(dataset, 'steps_per_epoch', steps_per_epoch)
         return dataset
 
 
-def run_acdml():
+def run_ccdml():
     test_database = ISICDatabase()
-    acdml = AttentionCrossDomainMetaLearning(
+    ccdml = CombinedCrossDomainMetaLearning(
         database=test_database,
         network_cls=MiniImagenetModel,
         n=5,
@@ -178,14 +172,14 @@ def run_acdml():
         number_of_tasks_val=100,
         number_of_tasks_test=1000,
         clip_gradients=True,
-        experiment_name='acdml',
+        experiment_name='cdml',
         val_seed=42,
         val_test_batch_norm_momentum=0.0,
     )
 
-    acdml.train(iterations=60000)
-    acdml.evaluate(100, seed=14)
+    ccdml.train(iterations=60000)
+    ccdml.evaluate(100, seed=14)
 
 
 if __name__ == '__main__':
-    run_acdml()
+    run_ccdml()
