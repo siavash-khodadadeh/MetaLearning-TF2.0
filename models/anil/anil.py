c6e4d25cabc60ff57ac61bf6a37a96f610de36cd
siavash-khodadadeh <siavash-khodadadeh@users.noreply.github.com>
2020-09-23 10:54:36 -0400

    Update anil.py
    Add a reference to the algorithm source.

--- models/anil/anil.py
+++ models/anil/anil.py
@@ -1,3 +1,14 @@
+# This is the implementation of ANIL algorithm proposed in 
+# Rapid learning or feature reuse? towards understanding the effectiveness of maml
+# by Aniruddh Raghu, Maithra Raghu, Samy Bengio, Oriol Vinyals.
+# Please cite this paper if you use this algorithm.
+# @article{raghu2019rapid,
+#   title={Rapid learning or feature reuse? towards understanding the effectiveness of maml},
+#   author={Raghu, Aniruddh and Raghu, Maithra and Bengio, Samy and Vinyals, Oriol},
+#   journal={arXiv preprint arXiv:1909.09157},
+#   year={2019}
+# }
+
 from models.maml.maml import ModelAgnosticMetaLearningModel
 
 
